a7419184cd32c98e0ddc81d7eed5493794325dda
macgeek30 <mike25ko@gmail.com>
2015-12-26 22:53:16 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-   
+
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
@@ -19,8 +19,8 @@ def create_project_hash
     }
   end
 
- 
-  projects  
+
+  projects
 end
 
 create_project_hash
