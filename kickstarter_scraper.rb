76de3f92d524c45241d1712bda59bc07b4390ccb
fossbender <fossbender@gmail.com>
2016-02-20 12:51:58 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,34 @@
 # require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css('li.project.grid_4').each do |project|
+    title = project.css('h2.bbcard_name strong a').text
+    projects[title.to_sym]={
+        :image_link => project.css('div.project-thumbnail a img').attribute('src').value,
+        :description => project.css('p.bbcard_blurb').text,
+        :location => project.css('ul.project-meta span.location-name').text,
+        :percent_funded => project.css('ul.project-stats li.funded strong').text.to_i
+    #learn used this: :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+  #individual project: # projects: kickstarter.css("li.project.grid_4")
+
+  #title: ^^use above to create `project` project.css("h2.bbcard_name strong a").text
+
+  #image link: project.css("div.project-thumbnail a img").attribute("src").value
+
+  #description: project.css("p.bbcard_blurb").text
+
+  #location:  project.css("ul.project-meta span.location-name").text
+
+  #%funded: project.css("ul.project-stats li.funded strong").text
+
+  projects
 end
