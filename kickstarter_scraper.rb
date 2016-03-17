2d7458e5b35cd3d8be4145f5246687454fd07840
CodeByLine <yleventhal@ymail.com>
2016-03-17 20:38:22 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,7 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
-binding.pry
+#binding.pry
   end
 
   # return the projects hash
