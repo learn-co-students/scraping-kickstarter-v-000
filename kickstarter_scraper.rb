f09e67c39acc631f5a188c4f43f41a923ab774d2
Michael Stewart <upp_mike@yahoo.com>
2016-06-28 18:00:28 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,19 +4,19 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-
-  listings = kickstarter.css(".project-card")
+  listings = kickstarter.css("li.project.grid_4")
+  #listings = kickstarter.css(".project-card")
   #binding.pry
 
   collection = {}
   listings.each do |listing|
     project = {
-      :image_link => listing.css("img").attribute("src").value,
-      :description => listing.css(".bbcard_blurb").text,
-      :location => listing.css(".location-name").text,
-      :percent_funded =>  listing.css(".funded").css("strong").text.chop.to_i
+      :image_link => listing.css("div.project-thumbnail a img").attribute("src").value,
+      :description => listing.css("p.bbcard_blurb").text,
+      :location => listing.css("ul.project-meta span.location-name").text,
+      :percent_funded =>  listing.css("ul.project-stats li.first.funded strong").css("strong").text.chop.to_i
     }
-    collection[(listing.css(".bbcard_name").css("a").text).to_sym] = project
+    collection[(listing.css("h2.bbcard_name strong a").text).to_sym] = project
   end
   #binding.pry
   collection
