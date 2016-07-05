0e7260ca17b5293cc78485c3cd7455f0e703b7c0
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-07-05 22:47:49 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,9 +17,8 @@ def create_project_hash
     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
-  puts projects
+  projects
 end
-create_project_hash
 
   #projects project = :kickstarter.css("li.project.grid_4")
 
