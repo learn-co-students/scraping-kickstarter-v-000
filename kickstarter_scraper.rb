e53cfd8bb9891c7e3b309d8c87c2f80eec8a3fbc
Malind Nikulski <malind.nikulski@gmail.com>
2016-09-02 19:03:12 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,12 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
+
+#projects: kickstarter.css("li.project.grid_4").first
+#title: project.css("h2.bbcard_name strong a").text
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
