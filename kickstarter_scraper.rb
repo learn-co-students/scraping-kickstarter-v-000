bf1c91a86f3b06b19eb7231f67486f3301cfa1e0
Juan Gongora <gongora.animations@gmail.com>
2016-11-04 23:19:59 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,19 +1,45 @@
 require 'nokogiri'
 require 'pry'
+
+# These are the scrapes needed to get us what we're looking for:
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
   projects = {}
+
+  # This code below created a massive hash of all sorts of key/value pairs:
+
+  # kickstarter.css("li.project.grid_4").each do |project|
+  #   projects[project] = {}
+
+  # This code below created the right set up but the rest of the project's values are not inside the title hash:
+
+  # kickstarter.css("li.project.grid_4").each do |project|
+  # title = project.css("h2.bbcard_name strong a").text
+  #   projects[title.to_sym] = {}
+
+  # This is the correct setup below:
+
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+        image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+        description: project.css("p.bbcard_blurb").text,
+        location: project.css("ul.project-meta span.location-name").text,
+        percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
   end
   projects
 end
 
-create_project_hash
+
+
+p create_project_hash
