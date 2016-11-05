6e35c8cc03499d42dbb7a224dd114b0fbaaaf7a3
Chenxi Wu <wuchenxi2013@gmail.com>
2016-11-05 09:22:26 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,19 @@
 # require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
+  r={}
+  doc=Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
+  doc.css(".project.grid_4").each do |proj|
+    r[proj.css("h2 strong a").text]={
+       image_link: proj.css("img.projectphoto-little").attribute("src").value,
+       description: proj.css("p").text,
+       location: proj.css(".location-name").text,
+       percent_funded: proj.css(".first funded strong").text.to_i
+    }
+  end
+  r
+
+
   # write your code here
 end
