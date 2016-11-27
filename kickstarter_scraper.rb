4e7df0bcae96cd9085532a6b6cc2dc95fe4589ba
Mary Umoh <umohm12@gmail.com>
2016-11-27 19:17:09 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,8 +14,5 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i}
     end
-
     projects
 end
-
-create_project_hash
