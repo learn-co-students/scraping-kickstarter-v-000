e180eec8825c39d48782bd5e336dbd24a54c660a
Andrea Toston <tostonar@gmail.com>
2017-03-16 22:30:53 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -22,8 +22,8 @@ def create_project_hash
     projects[title.to_sym] = {
       image_link: project.css("div.project-thumbnail a img").attribute("src").value,
       description: project.css("p.bbcard_blurb").text,
-      location: project.css("ul.project-meta li a").text,
-      percent_funded: project.css("li.first.funded strong").text.gsub("%", "").to_i
+      location: project.css("ul.project-meta li a span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
 
