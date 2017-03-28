dc8d228e9372665aeafa2b396ef099a2f31d5289
Sergio Mendoza <smendoza787@gmail.com>
2017-03-28 02:57:47 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,9 +21,3 @@ def create_project_hash
 end
 
 create_project_hash
-# projects: kickstarter.css("li.project.grid_4").first
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("span.location-name").text
-# percent funded: project.css("li.first.funded strong").text.gsub("%", "").to_i
