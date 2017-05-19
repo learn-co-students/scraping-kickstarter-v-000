8e2f1cf5edf92f29c3e2f0efaf4039171ac8cfb6
Amanda George <github email address>
2017-05-19 14:02:36 -0500

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,17 +18,7 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-
-  # return the projects hash
   projects
 end
-
-#projects: kickstarter.css("li.project.grid_4")
-#title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-#funded: project.css("ul.project-stats li.first.funded strong").text
-
 
 create_project_hash
