d24479cdaa252df6ffad70ad47b4822df39fe0bb
Noah Summers <summers.noah@gmail.com>
2017-06-14 20:44:13 -0400

    attempt refactor

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,19 @@
-# require libraries/modules here
+require "nokogiri"
 
 def create_project_hash
-  # write your code here
+  kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
+  project_hash = {}
+ 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2").text
+    project_hash[title.to_sym] = {
+      image_link: project.css("div.project-thumbnail img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+
+  project_hash
+  
 end
