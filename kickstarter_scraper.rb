2b65554ad7e945bcd0878dceae24459f67901ea6
Joanna Sese <jmgsese@gmail.com>
2017-08-25 15:02:06 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  projects = {}
+  projects = Hash.new
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
