8d9e2d83b19759f43bde1afb59a79dada05d861a
Caroline Shields <caroline.shields@amkor.com>
2017-09-07 21:23:52 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,7 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
+
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
