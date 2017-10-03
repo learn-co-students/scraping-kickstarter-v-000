1cd1da2d2159db54613c84a76044429157a93c6a
Sally Zasloff <szasloff@gmail.com>
2017-10-03 21:29:40 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -22,16 +22,6 @@ require 'pry'
 
   #Iterate through projects
 
-  # kickstarter.css("li.project.grid_4").each do |project|
-  #   title = project.css("h2.bbcard_name strong a").text
-  #   projects[title.to_sym] = {
-  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-  #     :description => project.css("p.bbcard_blurb").text,
-  #     :location => project.css("ul.project-meta span.location-name").text,
-  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  #   }
-  # end
-
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
