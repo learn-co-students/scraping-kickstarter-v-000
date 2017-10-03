836d37fc0bd0ae63d044f73bff3c9ef92881d6af
Sally Zasloff <szasloff@gmail.com>
2017-10-03 15:48:41 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
+require 'nokogiri'
+require 'pry'
 # require libraries/modules here
 
-def create_project_hash
-  # write your code here
-end
+  def create_project_hash
+
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  end
+binding.pry
+create_project_hash
+
+#projects:
+#kickstarter.css("li.project.grid_4")
