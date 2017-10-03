09b0ab687f8a84694304eba2e75cf135706fac22
Sally Zasloff <szasloff@gmail.com>
2017-10-03 21:28:34 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,32 +1,5 @@
 require 'nokogiri'
 require 'pry'
-# require libraries/modules here
-
-  def create_project_hash
-
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-
-  projects = {}
-
-
-  #Iterate through projects
-
-  kickstarter.css("lil.project.grid_4").each do |project|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
-  end
-
-  #return the projects hash
-
-  projects
-
-end
-
-
-  end
-
-create_project_hash
 
 #projects:
 #kickstarter.css("li.project.grid_4")
@@ -39,3 +12,36 @@ create_project_hash
 #project.css("span.location-name").text
 #percent funded:
 #project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
+
+  def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  #Iterate through projects
+
+  # kickstarter.css("li.project.grid_4").each do |project|
+  #   title = project.css("h2.bbcard_name strong a").text
+  #   projects[title.to_sym] = {
+  #     :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+  #     :description => project.css("p.bbcard_blurb").text,
+  #     :location => project.css("ul.project-meta span.location-name").text,
+  #     :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  #   }
+  # end
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+
+  #return the projects hash
+  projects
+  end
