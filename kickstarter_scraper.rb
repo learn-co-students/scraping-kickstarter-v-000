4a0d12be00b1c612ceda0b658523babb7c2d0e0d
Tony Distinti <adistinti@gmail.com>
2017-10-04 14:39:17 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,24 @@
 # require libraries/modules here
+#Remember - b/c the test website is a fake, static one, we don't need Open-Uri
+require 'nokogiri'
+require 'pry'
+
 
 def create_project_hash
   # write your code here
+
+  # This just opens a file and reads it into a variable
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+
+  # projects: kickstarter.css("li.project.grid_4")
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # description: project.css("p.bbcard_blurb").text
+  # location: project.css("ul.project-meta span.location-name").text
+  # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+  binding.pry
 end
+
+create_project_hash
