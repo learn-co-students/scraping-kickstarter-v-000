4ffeb93b338776e4b94983bbaad6dc09a0236642
alpbourne <alpbourne@gmail.com>
2017-10-05 14:38:22 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,6 +19,8 @@ def create_project_hash
   projects
 end
 
+create_project_hash
+
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
