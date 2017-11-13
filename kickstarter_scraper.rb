bfd06e73dd8aea9256de07d3e6e3e60c75bff2d6
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-13 06:26:18 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,6 +12,21 @@ def create_project_hash
   #location: project.css('ul.project-meta span.location-name').text
   #percent_funded: project.css('ul.project-stats li.first.funded strong').text.gsub('%',"").to_i
 
+  #project = kickstarter.css('li.project.grid_4')
+
+  projects = {}
+
+  kickstarter.css('li.project.grid_4').each do |project|
+    title = project.css('h2.bbcard_name strong a').text
+    projects[title.to_sym] = {
+      :image_link => project.css('.project-thumbnail a img').attribute('src').value,
+      :description => project.css('p.bbcard_blurb').text.strip,
+      :location => project.css('ul.project-meta span.location-name').text,
+      :percent_funded => project.css('ul.project-stats li.first.funded strong').text.gsub('%',"").to_i
+    }
+  end
+
+  projects
 
 end
 
