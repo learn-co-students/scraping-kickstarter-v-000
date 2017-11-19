cd828e948d7132da661a67d51b66d182034e497b
Olena Ageyeva <eageeva@hotmail.com>
2017-11-19 19:17:22 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,8 @@ def create_project_hash
   kickstarter=Nokogiri::HTML(html)
 
   #kickstarter.css("li.project.grid_4").first
+  # title: project.css("h2.bbcard_name strong a").text
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
   #binding.pry
 end
 
