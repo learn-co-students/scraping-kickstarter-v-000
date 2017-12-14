8aac68658da30155c71dedf711ce05074924d5ce
Charity <Youngblood.charity@gmail.com>
2017-12-13 17:58:42 -0800

    added nokogiri keyword selectors to kickstarter_scraper.rb

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,22 +4,24 @@ require 'nokogiri'
 
 def create_project_hash
   # write your code here
-  # each project has the following: 
-  project_hash = Hash.new
-    # image:image_link,
-    project_hash["image"] = "https://s3.amazonaws.com/ksr/projects/845788/photo-little.jpg?1391022013"
-    project_hash["description"] = kickstarter.css(".bbcard_blurb").first.css("p").text
-  binding.pry
-    # title:description, 
-    # description:string, 
-    # location:string 
-  
-
-  # set of key value pairs 
   
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
+  # each project has the following: 
+  project_hash = Hash.new
+    # image:image_link,
+    project_hash["image"] = kickstarter.css(".projectphoto-little").attribute("src").value
+    # title:???
+  
+    # description:string
+    project_hash["description"] = kickstarter.css(".bbcard_blurb").first.text
+    # location:string
+  binding.pry
+  
+  
+
+  # set of key value pairs 
  
 end
 
