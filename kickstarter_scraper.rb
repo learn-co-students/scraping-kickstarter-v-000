97ca6d51e5427f3ffc1bfdb03de9f9776599d198
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-03 22:58:51 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,13 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  #projects: kickstarter.css("li.project.grid_4")
+  binding.pry
 end
+
+
+
+create_project_hash
