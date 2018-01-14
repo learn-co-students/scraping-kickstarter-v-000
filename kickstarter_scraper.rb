3ac4772d03f2608ffcdf981c333942d342f24abf
JOSELY CASTRO <joselycas@gmail.com>
2018-01-14 20:30:40 +0000

    Issue with nokigori

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,7 @@
-# require libraries/modules here
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
 end
