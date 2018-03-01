3a16b8dc64d7601a7e534e3dd6faa09e3cfaf8c3
rons9n <rons9n@gmail.com>
2018-03-01 01:23:26 -0500

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,19 +1,19 @@
-# require libraries/modules here
-require "nokogiri"
-require "pry"
+# file: kickstarter_scraper.rb
 
-# projects = kickstarter.css("li.project.grid_4")
-# title = project.css("h2.bbcard_name strong a").text
-# img_link = project.css("div.project-thumbnail a img").attribute("src").value
-# description = project.css("p.bbcard_blurb").text
-# location = project.css(".location-name").text
-# percent_funded = project.css(".first.funded strong").text.gsub("%", "").to_i
+require 'nokogiri'
+require 'pry'
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
-  html = File.read("fixtures/kickstarter.html")
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  # binding.pry
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
@@ -21,12 +21,11 @@ def create_project_hash
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
-      :location => project.css(".location-name").text,
-      :percent_funded => project.css(".first.funded strong").text.gsub("%", "").to_i
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
 
+  # return the projects hash
   projects
 end
-
-create_project_hash
