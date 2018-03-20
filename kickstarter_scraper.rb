ab0edf5938042b7e0da92b58535e870a63b327a6
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-20 22:02:13 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,14 +5,17 @@ require 'open-uri'
 
 def create_project_hash
   result = {}
-  projects = Nokogiri::HTML(File.read("./fixtures/kickstarter.html")).css(".project-card")
+  file = File.read("./fixtures/kickstarter.html")
+  html = Nokogiri::HTML(file)
+  projects = html.css(".project-card")
   projects.each do |project|
     title = project.css("h2 strong a")[0].text
-    result[title] = {}
-    result[title][:image_link] = project.css(".project-thumbnail a img")[0].attributes['src'].value
-    result[title][:description] = project.css("p")[0].text
-    result[title][:location] = project.css(".location-name")[0].text
-    result[title][:percent_funded] = project.css(".funded strong")[0].text.to_i
+    result[title] = {
+      image_link: => project.css(".project-thumbnail a img")[0].attributes['src'].value
+      description: => project.css("p")[0].text
+      location: => project.css(".location-name")[0].text
+      percent_funded: => project.css(".funded strong")[0].text.to_i
+    }
   end
   result
 end
