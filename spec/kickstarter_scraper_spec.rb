3830c765dff919e46dfeb902abb36b4b75c04b6a
Brandon Brasson <rocksong622@gmail.com>
2018-03-22 19:45:10 +0000

    Done.

--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -1,4 +1,4 @@
-describe "#create_project_hash" do
+puts describe "#create_project_hash" do
 
   it "can be called on with no errors" do
     expect {create_project_hash}.to_not raise_error
@@ -21,25 +21,25 @@ describe "#create_project_hash" do
   it "each project has an image link hosted on AmazonAWS" do
     create_project_hash.each do |project_title, info_hash|
       regex = /https:\/\/s3.amazonaws.com\//
-      expect(regex.match(info_hash[:image_link])).to_not be_nil 
+      expect(regex.match(info_hash[:image_link])).to_not be_nil
     end
   end
 
   it "each project has a description which is a string" do
     create_project_hash.each do |project_title, info_hash|
-      expect(info_hash[:description].class).to eq(String) 
+      expect(info_hash[:description].class).to eq(String)
     end
   end
 
   it "each project has a location which is a string" do
     create_project_hash.each do |project_title, info_hash|
-      expect(info_hash[:location].class).to eq(String) 
+      expect(info_hash[:location].class).to eq(String)
     end
   end
 
   it "each project has percentage funded listed which is an integer" do
     create_project_hash.each do |project_title, info_hash|
-      expect(info_hash[:percent_funded].class).to eq(Fixnum) 
+      expect(info_hash[:percent_funded].class).to eq(Fixnum)
     end
   end
 
