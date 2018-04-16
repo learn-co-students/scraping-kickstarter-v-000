891b1139522bffca80159e74e8a7dbc28233be04
woollymammoth1 A <isaacatwood@outlook.com>
2018-04-16 21:31:10 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,22 @@
-# require libraries/modules here
+require 'nokogiri'
+
+#:projects => {
+#  "My Great Project"  => {
+#    :image_link => "Image Link",
+#    :description => "Description",
+#    :location => "Location",
+#    :percent_funded => "Percent Funded"
+#  },
+#  "Another Great Project" => {
+#    :image_link => "Image Link",
+#    :description => "Description",
+#    :location => "Location",
+#    :percent_funded => "Percent Funded"
+#  }
+#}
+
 
 def create_project_hash
-  # write your code here
+
+
 end
