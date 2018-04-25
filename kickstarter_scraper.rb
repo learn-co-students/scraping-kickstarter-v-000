f360da7edbef9d2e8b9780a58a1145d68d72cee8
eyi1 <ej7729@gmail.com>
2018-04-25 18:40:17 -0400

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,29 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
+  projects = {} 
+
+  #iterate through projects
+  kickstarter.css("li.project.grid_4").each do |project| 
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
+  end
+  projects
 end
+
+# projects: kickstarter.css("li.project.grid_4")
+  #kickstarter.css("li.project.grid_4").first (grabs the first project)
+# titles: project.css("h2.bbcard_name strong a").text
+# img: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# fund: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
