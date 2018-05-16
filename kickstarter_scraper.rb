aeec75011ba83d48b76b59f3c9e58930da69237f
Jonathan Dust <d24dust@hotmail.com>
2018-05-16 20:44:02 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,21 @@
 # require libraries/modules here
+require "nokogiri"
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css("#projects_list .project.grid_4").each do |project_card|
+    title = project_card.css(".bbcard_name strong a").text
+    projects[title.to_sym] = {
+      image_link: project_card.css(".project-thumbnail a img").attribute("src"),
+      description: project_card.css(".bbcard_blurb").text,
+      location: project_card.css(".project-meta span.location-name").text,
+      percent_funded: project_card.css(".project-stats .first.funded strong").text.to_i
+    }
+  end
+  projects
 end
--- spec/kickstarter_scraper_spec.rb
+++ spec/kickstarter_scraper_spec.rb
@@ -21,25 +21,25 @@ describe "#create_project_hash" do
   it "each project has an image link hosted on AmazonAWS" do
     create_project_hash.each do |project_title, info_hash|
       regex = /https:\/\/s3.amazonaws.com\//
-      expect(regex.match(info_hash[:image_link])).to_not be_nil 
+      expect(regex.match(info_hash[:image_link])).to_not be_nil
     end
   end
 
   it "each project has a description which is a string" do
     create_project_hash.each do |project_title, info_hash|
-      expect(info_hash[:description].class).to eq(String) 
+      expect(info_hash[:description].class).to eq(String)
     end
   end
 
   it "each project has a location which is a string" do
     create_project_hash.each do |project_title, info_hash|
-      expect(info_hash[:location].class).to eq(String) 
+      expect(info_hash[:location].class).to eq(String)
     end
   end
 
   it "each project has percentage funded listed which is an integer" do
     create_project_hash.each do |project_title, info_hash|
-      expect(info_hash[:percent_funded].class).to eq(Fixnum) 
+      expect(info_hash[:percent_funded].class).to eq(Fixnum)
     end
   end
 
