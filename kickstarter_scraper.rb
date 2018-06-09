97ad025d30bae30400205d9cbb8835ea1f9882f2
Diogo Ide <diogoide@gmail.com>
2018-06-09 18:42:51 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,12 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  Kickstarter = Nokogiri::HTML(html)
+
 end
+
+
+binding.pry
