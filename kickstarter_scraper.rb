23f057aa4df0d03c8feb29973f9ead91109c5b33
Diogo Ide <diogoide@gmail.com>
2018-06-09 18:55:37 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,10 +3,11 @@ require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-
-  Kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
 
 end
 
 
 binding.pry
+
+create_project_hash
