ca12eb52307d1a79c52a47b99cef298c2b9276aa
Jeremy Schuurmans <jeremyschuurmans@gmail.com>
2018-06-28 19:01:39 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,9 +23,5 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
-
   projects
-
 end
-
-create_project_hash
