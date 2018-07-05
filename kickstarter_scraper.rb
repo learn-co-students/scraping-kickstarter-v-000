4021ba59a3be32ae92aceb9730da646eec8e0d28
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-05 12:18:10 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
-# require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
 end
+
+binding.pry
+create_project_hash
