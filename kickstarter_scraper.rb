0e02e30f29c5a6ed7c5b2e208bb90d0b4f245e5e
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-07 21:15:02 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text
-## percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+#percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
 create_project_hash
