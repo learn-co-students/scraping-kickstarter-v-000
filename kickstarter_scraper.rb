20b889d779e3b16d08cf566be7e34a08836f743a
Leslie Stevens <flylady@mail.utexas.edu>
2018-07-07 19:54:57 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,8 +5,12 @@ require 'pry'
     html = File.read('fixtures/kickstarter.html')
 
     kickstarter = Nokogiri::HTML(html)
-    puts kickstarter.css("li.project.grid_4").first
+    binding.pry
+    #puts kickstarter.css("li.project.grid_4").first
   end
-
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
 
 create_project_hash
