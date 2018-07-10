557499f493cb0095e10cba88739051d607a0a40d
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-10 22:17:49 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
 # require libraries/modules here
+require 'nokogiri'
+
 
 def create_project_hash
-  # write your code here
+  html = File.read("fixtures/kickstarter.html")
+
+  kickstarter = Nokogiri::HTML(html)
 end
