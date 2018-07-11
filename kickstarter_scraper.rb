aee784728aa05343fefb4082939c634ee1c42c37
Trisha Garrett <trisha.garrett@gmail.com>
2018-07-11 01:09:06 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,4 +6,7 @@ def create_project_hash
   html = File.read("fixtures/kickstarter.html")
 
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
+
+  # projects: kickstarter.css("li.project.grid_4")
 end
