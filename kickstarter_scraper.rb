57d31fd29b8665886814c27afc0f839d8f30985e
Laura Messa Travin <laura.travin@gmail.com>
2018-07-12 20:47:49 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -30,3 +30,12 @@ def create_project_hash
   #  binding.pry
     return project
 end
+
+
+#flatiron scraping
+#projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
