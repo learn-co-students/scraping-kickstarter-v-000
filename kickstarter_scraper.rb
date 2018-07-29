28725342f519ee23869d33875841b0c855dbef57
Reno Sorensen <renojames19@gmail.com>
2018-07-29 22:01:18 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,15 +2,19 @@ require 'nokogiri'
 require 'pry'
 
 def create_project_hash
-
   html = File.read('fixtures/kickstarter.html')
-
   kickstarter = Nokogiri::HTML(html)
 
-  binding.pry
 
 end
 
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta li a span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 
 create_project_hash
