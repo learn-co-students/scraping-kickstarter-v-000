f8742df10062ac0a109ab3730fd33578bb1a84cc
Aziz Kosber <a.kosber@gmail.com>
2018-08-11 14:36:37 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,27 +5,27 @@
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text
 #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
-    html = File.read('fixtures/kickstarter.html') #if you were using open-uri this is wehre you'd open the webpageand get all the htmla data.
-    kickstarter = Nokogiri::HTML(html) #passes in your html as an argument to Nokogiri::HTML which creates XML elements that you can iterate over.
+
+  def create_project_hash
+    html = File.open('fixtures/kickstarter.html')
+    kickstarter = Nokogiri::HTML(html)
 
     projects = {}
 
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
       projects[title.to_sym] = {
-        image_link: project.css("div.project-thumbnail a img").attribute("src").value,
-        description: project.css("p.bbcard_blurb").text,
-        location: project.css("ul.project-meta span.location-name").text,
-        percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-      }
-
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("ul.project-meta span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+    }
     end
-
     projects
-end
+  end
 
 create_project_hash
