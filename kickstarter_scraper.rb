74d4e717465cb54d0be3a9948a403bacb781c3c2
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-14 20:49:55 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,18 @@ def create_project_hash
   # percent_funded: # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
+  end
+  projects
 end
 
 create_project_hash
