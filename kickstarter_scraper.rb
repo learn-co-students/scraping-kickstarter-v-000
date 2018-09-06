6ee6e23fb48a087d36248dea26019b0513e2aedb
Elizabeth Karst <efkarst@outlook.com>
2018-09-06 02:39:27 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,22 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  #grab project list from HTML
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  project_list = kickstarter.css("li.project.grid_4")
+
+  #scrape for project info and add to hash
+  projects = {}
+  project_list.each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text.chomp,
+      location: project.css("ul.project-meta li a span.location-name").text,
+      percent_funded: project.css("ul.project-stats li.first.funded").text.gsub("%","").to_i
+    }
+  end
+  projects
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,4 @@
-require_relative '../kickstarter_scraper'
+require_relative '../kickstarter_scraper.rb'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
