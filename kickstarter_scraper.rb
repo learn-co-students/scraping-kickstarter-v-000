fe1d47171ab72f3ffafe719a8506ceda2a6002a6
sharonrieger <sharonarieger@gmail.com>
2018-09-06 13:02:20 -0400

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,10 @@ def create_project_hash
 html = File.read('fixtures/kickstarter.html')
 kickstarter = Nokogiri::HTML(html)
 projects = {}
+ # Iterate through the projects
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
+    #converting the title into a symbol w/_sym method. Symbols make better hash keys than strings.
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
@@ -15,6 +17,7 @@ projects = {}
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
+  # return the projects hash
   projects
 end
 
