8ea3cd9493f268b709973c471abb65c8fa101c7c
Angie Kassab <anasab12@gmail.com>
2018-09-09 20:27:45 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -34,7 +34,7 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   project_list = kickstarter.css("li.project.grid_4")
 
-  #scrape for project info and add to hash
+  #scrape for project info and add to hash.These individual project hashes will be collected into a larger hash called projects.
   projects = {}
   project_list.each do |project|
     title = project.css("h2.bbcard_name strong a").text
