856b48fc55f50fcb4cf033b211546d024ac4d7be
Ashley Wong <ashwong04@gmail.com>
2018-10-09 01:38:40 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,10 @@
 # require libraries/modules here
 
+require 'nokogiri'
+html = File.read('fixtures/kickstarter.html')
+
+kickstarter = Nokogiri::HTML(html)
+
 def create_project_hash
   # write your code here
 end
