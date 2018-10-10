81e4f3c645a4253a25dbac6fa556a1adfdbdfb16
AT Williams <aderiannaw@yahoo.com>
2018-10-10 02:30:19 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,3 @@
-# require libraries/modules here
 
 require 'nokogiri'
 
@@ -23,11 +22,3 @@ def create_project_hash
  
 
 
-
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-
-# location: project.css("span.location-name").text
-#percent funded:  project.css("li.first.funded strong").text.gsub("%", "").to_i
