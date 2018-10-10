b7f651292b4a88444c1bb88121a0cae295e4a5fe
Ashley Wong <ashwong04@gmail.com>
2018-10-10 13:18:16 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,8 +10,14 @@ require 'pry'
    
     kickstarter.css("li.project.grid_4").each do |project|
       title = project.css("h2.bbcard_name strong a").text
-      projects[title.to_sym] = {}
+      projects[title.to_sym] = {
+        :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+        :description => project.css("p.bbcard_blurb").text,
+        :location => project.css("ul.project-meta span.location-name").text,
+        :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+        }
     end
+    projects
   end
 
 
