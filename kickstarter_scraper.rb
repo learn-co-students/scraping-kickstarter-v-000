da01cb7a8112f2e716601c076b7844cbc3894963
Malcome Fein <mfein90@gmail.com>
2018-11-14 21:13:13 -0500

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,12 @@
 # file: kickstarter_scraper.rb
 
+# Steps
+# 1. Get html from nokogiri - it turns html into xml
+# 2. set the output from nokogiri into a variable eg. kickstarter
+# 3. Create a empty hash
+# 4. use the .method of the css selector on the kickstarter variable and itterate over the collection
+# i.e the project
+
 require 'nokogiri'
 require 'pry'
 
@@ -11,9 +18,10 @@ require 'pry'
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 
 def create_project_hash
+  # This just opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+  # empty hash to store scraped data
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
@@ -24,8 +32,13 @@ def create_project_hash
       :location => project.css("ul.project-meta span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
+    #binding.pry
   end
 
   # return the projects hash
   projects
+
 end
+
+# binding.pry
+# puts create_project_hash
