15c2d87917455c3c3fd4c5f401c2774bcc06ec01
Katie Moosman <moosman.katie@gmail.com>
2019-01-03 04:35:36 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,10 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value, :description => project.css("p.bbcard_blurb").text, :location => project.css("ul.project-meta span.location-name").text,       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
  
