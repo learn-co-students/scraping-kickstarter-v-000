f52d5870b2712b8fd8aacccf53c62445c857e08d
Dan Suarez <suarez.daniel101@gmail.com>
2019-07-03 04:37:51 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,13 @@
 require 'nokogiri'
 require 'pry'
 
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
@@ -11,10 +18,3 @@ end
 
 
 create_project_hash
-
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("span.location-name").text
-# percent: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
