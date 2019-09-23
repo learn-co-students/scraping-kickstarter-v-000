30700ce2916b47d0d984d9f042de9df9458a6868
Olivia Kesler <oakesler@protonmail.ch>
2019-09-23 18:06:22 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,18 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
+
+#projects: kickstarter.css("li.project.grid_4")
+
+# title: project.css("h2.bbcard_name strong a").text
+
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+
+# description: project.css("p.bbcard_blurb").text
