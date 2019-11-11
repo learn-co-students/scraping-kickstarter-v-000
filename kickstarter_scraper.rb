0c9fd85dac06ab44f9f1f182d61aa45e8c20868c
Luis Herrera <herrera.lrh@gmail.com>
2019-11-11 19:57:09 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -16,14 +16,12 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
-    projects
+  projects
 end
-
-create_project_hash
-
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-# location: project.css("span.location-name").text
+# location: project.css("ul.project-meta li a span").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+create_project_hash
