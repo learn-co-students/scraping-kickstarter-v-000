ee5e61d97224bf61c4879da39e1d36d418f51bdb
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-03 13:43:11 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,4 @@
-require 'nokogiri'
+require "nokogiri"
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -10,11 +10,21 @@ def create_project_hash
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
+      :description => project.css("p.bbcard_blurb").text.strip,
+      :location => project.css("ul.project-meta li").text.strip,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
   end
 
   projects
+
 end
+
+create_project_hash
+
+#projects: kickstarter.css("li.project.grid_4")
+#title: project.css("h2.bbcard_name strong a").text
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
+#description: project.css("p.bbcard_blurb").text.strip
+#location: project.css("ul.project-meta li").text.strip
+#percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
